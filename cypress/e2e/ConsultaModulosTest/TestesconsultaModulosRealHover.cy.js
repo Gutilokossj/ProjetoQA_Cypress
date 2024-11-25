9364191b3a603053e842623517c8d223392e4634
Gutilokossj <gutilokossj12@gmail.com>
2024-11-25 16:51:35 -0300

    Ajuste HomeTest.cy.js, Teste editor, forçando por enquanto

--- cypress/e2e/ConsultaModulosTest/TestesconsultaModulosRealHover.cy.js
+++ cypress/e2e/ConsultaModulosTest/TestesconsultaModulosRealHover.cy.js
@@ -7,5 +7,13 @@ describe('Teste realhover', () => {
     it('Testando RealHover no consultaMódulos', () => {
         cy.get('#consultarModulosBtn').realHover()
     });
+
+    it('Outro teste RealHover, elemento que aparece', () => {
+        cy.get('#cnpj').type('01.120.191/0001-98')
+        cy.get('#consultarModulosBtn').click()
+        cy.url('equal', 'https://consulta-bkp.vercel.app/resultado.html?cnpj=01120191000198')
+        cy.get('.licencaValue').should('be.visible')
+        .realHover()
+    });
     
 });
