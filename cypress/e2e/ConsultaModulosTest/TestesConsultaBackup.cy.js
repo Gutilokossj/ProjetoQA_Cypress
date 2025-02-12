bc42630cbf734fad5e4dc13321d61e6d4402e018
Gutilokossj <gutilokossj12@gmail.com>
2025-02-12 12:35:46 -0300

    Ajustes TestesConsultaBackup.cy.js - ok

--- cypress/e2e/ConsultaModulosTest/TestesConsultaBackup.cy.js
+++ cypress/e2e/ConsultaModulosTest/TestesConsultaBackup.cy.js
@@ -16,6 +16,7 @@ describe('Teste consultas possíveis para o usuário', () => {
       cy.get('#cnpj').type('03.008.433/0001-81');
       cy.get('#consultaBKP').click()
       cy.url().should('equal','http://127.0.0.1:5500/docs/resultado_BKP.html?cnpj=03008433000181');
+      cy.wait(2500)
     })
 
     it('Cliente SIEM - CNPJ - TESTE RESET ENVIO XML (SEM RESETAR)', () => {
@@ -26,25 +27,26 @@ describe('Teste consultas possíveis para o usuário', () => {
 
       // Espera até que o botão #resetaXML esteja visível antes de clicar
       cy.get('#resetaXML').should('be.visible').click();
+      cy.wait(1000)
       // Espera até que o botão #cancelReset esteja visível antes de clicar
       cy.get('#cancelReset').should('be.visible').click();
   });
 
       it('Cliente GA - CNPJ', () => {
-        cy.get('#toggleButton').click();
         cy.get('#cnpj').type('10.738.886/0001-29');
         cy.get('#consultaBKP').click();
         cy.url().should('equal','http://127.0.0.1:5500/docs/resultado_BKP.html?cnpj=10738886000129');
+        cy.wait(2500)
       });
 
       it('Cliente GA - CNPJ TESTE RESET ENVIO XML (SEM RESETAR)', () => {
-        cy.get('#toggleButton').click();
         cy.get('#cnpj').type('10.738.886/0001-29');
         cy.get('#consultaBKP').click();
         cy.url().should('equal','http://127.0.0.1:5500/docs/resultado_BKP.html?cnpj=10738886000129');
         cy.wait(1000)
   
         cy.get('#resetaXML').should('be.visible').click();
+        cy.wait(1000)
         cy.get('#cancelReset').should('be.visible').click();
       });
 
